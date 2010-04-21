dbb037e028466d86d23d167e83467a14cc15e42e
bjb322@drexel.edu <bjb322@drexel.edu@41fb5051-a1ef-c4d5-7a4a-1a0519da2a45>
2010-04-21 01:45:23 +0000

    Added a form for submitting problem solutions, and a page to view the results from that form.

---
+++ src/bugkillr/PrintSubmissionServlet.java
@@ -0,0 +1,34 @@
+package bugkillr;
+import java.io.IOException;
+import java.util.Enumeration;
+
+
+
+import javax.servlet.http.*;
+import html.HTMLWriter;
+/**
+ * @author Randy Bohrer
+ * This servlet exists as a debugging tool for the problem submission system.
+ * It prints the content of a submitted problem.
+ */
+@SuppressWarnings("serial")
+public class PrintSubmissionServlet extends HttpServlet {
+	@SuppressWarnings("unchecked")
+	public void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+		HTMLWriter hw = new HTMLWriter(req, resp);
+		hw.writeProlog("Bugkiller - View Problem Submission");
+		hw.writeHeader();
+		
+		resp.getWriter().println("Viewing submission from " + req.getUserPrincipal().getName()+" <br/>\n" +
+				"Problem ID = " + req.getParameter("pid")+"<br/>\n" +
+				"Source Code =" + req.getParameter("inputText") + "<br/>\n" );
+	    Enumeration paramNames = req.getParameterNames();
+	    while(paramNames.hasMoreElements())
+	    {
+	    	resp.getWriter().println("PARAM " + paramNames.nextElement() + "<br/>");
+	    }
+	    
+		hw.writeEpilog();
+		//req.getParameter("inputtext")
+	}
+}
