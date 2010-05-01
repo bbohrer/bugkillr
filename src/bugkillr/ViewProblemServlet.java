b792ac6b9863006018134c107e53cc106e731b03
bjb322@drexel.edu <bjb322@drexel.edu@41fb5051-a1ef-c4d5-7a4a-1a0519da2a45>
2010-05-01 04:51:35 +0000

    Changed ViewUnsolvedProblems (which is now an inaccurate name, should be changed to ViewAvailableProblems) to show a list of problems based on the minimum score required to view them. Updated problem class to include a minimum score. Updated "create problem" form to include this field. Updated user class to have a score field.
    Created "View Problem" page that incorporates a problem's description and has links to the hints and submission page.
    (Please excuse the names of the problems I created. I get very silly when I'm debugging).
    
    Previous commits gave me errors so here's trying to fix them.

---
+++ src/bugkillr/ViewProblemServlet.java
@@ -0,0 +1,74 @@
+package bugkillr;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.List;
+
+import javax.jdo.PersistenceManager;
+import javax.jdo.Query;
+import javax.servlet.http.*;
+
+import html.HTMLWriter;
+import redirects.Redirector;
+import bugkillr.PMF;
+
+@SuppressWarnings("serial")
+public class ViewProblemServlet extends HttpServlet {
+	@SuppressWarnings("unchecked")
+	public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+		PersistenceManager pm = PMF.get().getPersistenceManager();
+		HTMLWriter hw = new HTMLWriter(req, resp);
+		Redirector redir = new Redirector(req,resp);
+		hw.writeProlog("Bugkiller - View Problem");
+		redir.loginRedirect();
+		long pid = 0;
+		Problem curProblem = null;
+		User curUser = null;
+		try {
+			redir.userRedirect();
+			pid = Integer.parseInt(req.getParameter("pid"));
+			curUser = redir.getUserFromDatastore();
+			Query q = pm.newQuery("select from " + Problem.class.getName() + " where key == pid parameters long pid");
+			List<Problem> probs = (List<Problem>) q.execute(pid);
+			if(probs.size() > 1)throw new Exception("Multiple problems found for this ID");
+			if(probs.size() == 0)throw new Exception("Problem not found");
+			curProblem = probs.get(0);
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		hw.writeHeader();
+		if(curProblem.getMinscore() > curUser.getScore())
+		{
+			resp.getWriter().println("Error: You do not have the score required to view this problem.");
+		}
+		else
+		{
+			resp.getWriter().println("<h1>View Problem</h1>");
+			resp.getWriter().println("<h2>Problem Name: "+ curProblem.getName()+"</h2>");
+			resp.getWriter().println("<h3>Current Score: " + curUser.getScore() + "</h3>");
+			resp.getWriter().println("<h3>Description</h3>");
+	
+			URL url = new URL(curProblem.getDescriptionURL());
+			HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+			connection.setDoOutput(true);
+			connection.setRequestMethod("GET");
+			String line = null;
+			if (connection.getResponseCode() == HttpURLConnection.HTTP_OK) {
+				BufferedReader reader = new BufferedReader(new InputStreamReader(url.openStream()));
+				while((line = reader.readLine()) != null)
+					resp.getWriter().println(line);
+			}
+			else
+			{
+					resp.getWriter().println("Error: Could not open description file.");
+			}
+			hw.writeLink( curProblem.getHelpURL() ,"View hints for this problem.");
+			hw.writeLink("submitform?pid=" + pid, "Submit your solution for this problem.");
+			hw.writeEpilog();
+		}	
+	}
+}
